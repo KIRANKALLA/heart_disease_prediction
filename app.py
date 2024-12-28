9197c7cf5466c347363515d799200835be4db4da
KIRANKALLA <kallakiran1974@gmail.com>
2024-12-28 12:52:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+m = joblib.load('e:/datasets/hdp_model.joblib')
+
+st.title('Heart Disease Prediction')
+st.write('Enter Features such as Age,Gender Etc;;')
+
+a = st.number_input('Enter Age')
+b = st.number_input('Enter Cigarettes Per Day')
+c = st.number_input('Total Cholestrol')
+d = st.number_input('Enter Systolic BP')
+e = st.number_input('Enter Diastolic BP')
+f = st.number_input('Enter BODY MASS INDEX')
+g = st.number_input('Enter Heart Rate')
+h = st.number_input('Enter Glucose')
+
+if st.button('Predict'):
+    p = m.predict([[a,b,c,d,e,f,g,h]])
+    if p==0:
+        st.write('You Will Have Heart Disease in the Next 10 years')
+    else:
+        st.write('You Will not Have Heart Disease in the Next 10 Years')
+
